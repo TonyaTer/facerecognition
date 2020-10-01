04bb6f30d32545006c0a89fe378ff64084ce6296
TonyaTer <65990995+TonyaTer@users.noreply.github.com>
2020-10-01 12:22:35 -0400

    added nav link and logo file

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,10 @@
+import { logDOM } from '@testing-library/react';
+import React from 'react';
+
+const Logo = () =>{
+    return(
+        <div className='ma4 ma0'></div>
+    );
+}
+
+export default Logo;
